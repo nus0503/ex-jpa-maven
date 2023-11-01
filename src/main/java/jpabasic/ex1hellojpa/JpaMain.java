8cfc261eba83425cdf818df9feaed21da6165e07
kjs <nus0503@naver.com>
2023-11-02 01:41:42 +0900

    main method

---
+++ src/main/java/jpabasic/ex1hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package jpabasic.ex1hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //영속성 컨텍스트(persistence context)에는 1차 캐시와 2차 캐시가 있는데
+        //1차 캐쉬는 데이터베이스 한 트랙잭션 안에서만 사용이 가능하다.
+        //2차 캐쉬는 애플리케이션 전체에서 공유하는 캐시다.
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+//            Member member = em.find(Member.class, 150L);
+            Member member = new Member();
+            member.setId(1L);
+            member.setUsername("user");
+            member.setRoleType(RoleType.USER);
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
