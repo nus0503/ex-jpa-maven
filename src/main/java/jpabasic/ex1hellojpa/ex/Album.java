a05afc86fbb0fad9c8313174e1efee9ce3ddc3a7
KJS <nus0503@naver.com>
2023-11-07 17:22:14 +0900

    DELETE

--- src/main/java/jpabasic/ex1hellojpa/ex/Album.java
+++
@@ -1,9 +0,0 @@
-package jpabasic.ex1hellojpa.ex;
-
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-@DiscriminatorValue(value = "A")
-@Entity
-public class Album  extends Item{
-    private String artist;
-}
