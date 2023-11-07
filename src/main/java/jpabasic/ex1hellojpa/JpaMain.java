7ce715b995798448195b37d843fa5f64f0a12e0a
kjs <nus0503@naver.com>
2023-11-07 23:51:02 +0900

    즉시 로딩의 문제점

--- src/main/java/jpabasic/ex1hellojpa/JpaMain.java
+++ src/main/java/jpabasic/ex1hellojpa/JpaMain.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 
 public class JpaMain {
@@ -25,13 +26,18 @@ public class JpaMain {
             Team team = new Team();
             team.setName("teamA");
             em.persist(team);
-
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
 
             Member member1 = new Member();
             member1.setName("member1");
             member1.setTeam(team);
             em.persist(member1);
-
+            Member member2 = new Member();
+            member2.setName("member2");
+            member2.setTeam(team2);
+            em.persist(member2);
 
 
 
@@ -39,12 +45,9 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
-            System.out.println("m = " + m.getTeam().getClass());
-
-            System.out.println("==================");
-            m.getTeam().getName(); //<-  fetch = FetchType.LAZY일 시 프록시 객체엔 필드의 값들이 없기 때문에 getName()같은 메소드로 값들을 이용하기 위해 동작할 때 초기화가 일어난다.
-            System.out.println("==================");
+            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList(); // 즉시로딩 했을 때 JPQL에서의 N + 1문제
+            // 이 로직은 모든 멤버를 가지고 오는 로직이다. 근데 만약 멤버 테이블에 2개의 row가 있다면 일단 member 쿼리는 한번만 나간다.
+            // 하지만 member필드엔 team도 있기에 각각의 row의 해당하는 team을 찾기 위해 쿼리가 실행된다. 이 과정에선 member 쿼리 1번, team 쿼리 2번이 나간다.
 
             tx.commit();
         } catch (Exception e) {
